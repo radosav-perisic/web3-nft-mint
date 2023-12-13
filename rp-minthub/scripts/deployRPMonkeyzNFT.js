6f7ccc71ee2c1734ae50986fc2decf7fa17a78c1
Radosav Perisic <radosavperisic00@gmail.com>
2023-12-13 14:49:25 +0100

    Finalize Smart Contract Setup and Configuration

--- rp-minthub/scripts/deployRPMonkeyzNFT.js
+++ rp-minthub/scripts/deployRPMonkeyzNFT.js
@@ -7,22 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+ const RPMonkeyzNFT = await hre.ethers.getContractFactory("RPMonkeyzNFT");
+ const RPmonkeyzNFT = await RPMonkeyzNFT.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+ await RPmonkeyzNFT.deployed();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+ console.log("RPMonkeyzNFT deployed to:", RPMonkeyzNFT.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
